85ca55403944f32f275be73b14d215a8ddcdb964
Geniyk <124154446+Geniyk@users.noreply.github.com>
2023-11-15 15:37:39 +0530

    Add files via upload

---
+++ Code.ino
@@ -0,0 +1,82 @@
+
+#define BLYNK_TEMPLATE_ID "TMPL3o-C2oO-o"
+#define BLYNK_DEVICE_NAME "GPS Module"
+#define BLYNK_AUTH_TOKEN "T7Skc_8VRK10MkSTiPUtFwWPAIRH89mJ"
+
+char auth[] = BLYNK_AUTH_TOKEN;
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "Redmi5G";
+char pass[] = "123456788";
+
+#define BLYNK_PRINT Serial
+
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <BlynkSimpleEsp32.h>
+
+//GPS Module Settings
+//GPS Module RX pin to ESP32 17
+//GPS Module TX pin to ESP32 16
+#include <TinyGPSPlus.h> 
+#define RXD2 16
+#define TXD2 17
+HardwareSerial neogps(2);
+TinyGPSPlus gps;
+
+BlynkTimer timer;
+
+#define INTERVAL 1000L
+
+void sendGps()
+{
+  while(neogps.available())
+  {
+    if (gps.encode(neogps.read()))
+    {
+      break;
+    }
+  }
+  if (!gps.location.isValid())
+  {
+    Serial.println("Failed to read True location from GPS Module!");
+    return;
+  }
+  
+  //get latitude and longitude
+  float latitude = gps.location.lat();
+  float longitude = gps.location.lng();
+  float speed = gps.speed.kmph();
+  
+  Serial.print("Latitude:  ");
+  Serial.println(latitude, 6);
+  Serial.print("Longitude: ");
+  Serial.println(longitude, 6);
+  Serial.print("Speed: ");
+  Serial.println(speed, 6);
+  
+  Blynk.virtualWrite(V1, String(latitude, 6));
+  Blynk.virtualWrite(V2, String(longitude, 6));
+  Blynk.virtualWrite(V0, String(speed));
+ 
+}
+
+void setup()
+{
+ 
+  Serial.begin(115200);
+
+  Blynk.begin(auth, ssid, pass);
+ 
+  neogps.begin(9600, SERIAL_8N1, RXD2, TXD2);
+  Serial.println("neogps serial initialize");
+  delay(10);
+  
+  timer.setInterval(INTERVAL, sendGps);
+ 
+}
+
+void loop(){
+  Blynk.run();
+  timer.run();
+}
